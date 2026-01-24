34767d4c2619be091f0e1d56ec1a44a8f665cc91
PhrimeUIUX <ejdomingo82@gmail.com>
2026-01-24 11:20:57 +0800

    added: about navigation bar

---
+++ script/navigationbar.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById("navToggle");
+const drawer = document.getElementById("navDrawer");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("open");
+  drawer.classList.toggle("open");
+});
