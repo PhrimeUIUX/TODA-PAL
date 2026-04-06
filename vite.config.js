c52c3b26addd136513c9dac1f932aa42a45d535e
PhrimeUIUX <ejdomingo82@gmail.com>
2026-04-06 11:18:07 +0800

    fixed visual height and centering of each section

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react(), basicSsl()],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        https: {}
+    }
 });
